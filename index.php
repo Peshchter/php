4cd76ca098aa582ead2f52a4e697b96a3bfc3b44
Pavel <leniviy.putnik@gmail.com>
2019-06-20 16:43:00 +0400

    ДЗ для 4  урока

--- index.php
+++ index.php
@@ -2,21 +2,53 @@
 
 <?php
 $lang = "ru";
-$title = "Главная страница";
+$title = "ДЗ 4";
 $today = getdate();
 ?>
 
 
-<html lang=<?=$lang?>>
+<html lang=<?= $lang ?>>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?=$title ?></title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title><?= $title ?></title>
 </head>
 <body>
-    <h1>Текущее время: <?php $today['hours'] ?> часов, <?php $today['minutes']?> минут, <?php $today['seconds']?> секунд.</h1>
-    <h1>Дата: <?php $today['mday']?>.<?php $today['month']?>.<?php $today['year']?></h1>
+<?php
+$excludes = [".", ".."];
+$images = scandir('images');
+//var_dump($images);?>
+
+<div style="display: flex; flex-wrap: wrap; flex-direction: row;">
+    <?php foreach ($images as $img) {
+        if (!in_array($img, $excludes)) { ?>
+            <a href="<?= 'images/' . $img; ?>" target="_blank">
+                <div class="card" style="width: 18rem;">
+                    <img src="<?= 'images/' . $img; ?>" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title"> Имя файла </h5>
+                        <p class="card-text"> <?= $img; ?></p>
+                    </div>
+                </div>
+            </a>
+            <?php
+        }
+    }
+    ?>
+</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
 </body>
+
 </html>
